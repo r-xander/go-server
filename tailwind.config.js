16aedac9d2c0b11d8ec61e8ea336f50e1f226c57
Ryan <ryanm_alex@yahoo.com>
2024-02-05 21:49:16 -0500

    shifted to mostly flex layout for form_designer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./views/*.html", "./js/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            gridTemplateColumns: {
+                "fill-18": "repeat(auto-fill, minmax(4.5rem, 1fr))",
+            },
+        },
     },
     plugins: [],
 };
