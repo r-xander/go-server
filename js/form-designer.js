aaaf6b4f70bc17e4057ea66362996fce6f804d27
Ryan <ryanm_alex@yahoo.com>
2024-02-13 21:34:54 -0500

    initial commit for form designer global object

---
+++ js/form-designer.js
@@ -0,0 +1,68 @@
+(function (root, func) {
+    root.formDesigner = root.formDesigner || func();
+})(typeof self !== "undefined" ? self : this, function () {
+    return (function () {
+        "use strict";
+
+        const formDesigner = {
+            templates: {},
+            newField: newField,
+        };
+
+        function newField(parentSelector, type) {
+            const template = formDesigner.templates[type];
+            const field = template.cloneNode(true);
+
+            const parent = document.querySelector(parentSelector);
+            parent.appendChild(field);
+
+            const child = /** @type {HTMLDivElement} */ (target?.lastElementChild);
+            child.style.opacity = "0";
+            child.style.translate = "-12px 0";
+            window.getComputedStyle(child).opacity;
+            child.removeAttribute("style");
+        }
+
+        function intializeTemplates() {
+            const templates = getDocument().querySelectorAll("[form-template]");
+
+            for (let i = 0; i < templates.length; i++) {
+                const template = templates[i];
+                const templateName = template.getAttribute("form-template");
+
+                formDesigner.templates[templateName] = template;
+            }
+        }
+
+        //====================================================================
+        // Initialization
+        //====================================================================
+        var isReady = false;
+        getDocument().addEventListener("DOMContentLoaded", function () {
+            isReady = true;
+        });
+
+        /**
+         * Execute a function now if DOMContentLoaded has fired, otherwise listen for it.
+         *
+         * This function uses isReady because there is no realiable way to ask the browswer whether
+         * the DOMContentLoaded event has already been fired; there's a gap between DOMContentLoaded
+         * firing and readystate=complete.
+         */
+        function ready(fn) {
+            // Checking readyState here is a failsafe in case the htmx script tag entered the DOM by
+            // some means other than the initial page load.
+            if (isReady || getDocument().readyState === "complete") {
+                fn();
+            } else {
+                getDocument().addEventListener("DOMContentLoaded", fn);
+            }
+        }
+
+        ready(function () {
+            intializeTemplates();
+        });
+
+        return formDesigner;
+    })();
+});
